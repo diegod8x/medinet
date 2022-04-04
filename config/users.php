7ed7b95a84c11f072fbf9afdaa274dba43893390
Diego Diaz <diego.leopoldo.diaz@gmail.com>
2022-04-03 22:44:39 -0400

    incio admin y users

---
+++ config/users.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'Users.Registration.active' => true,
+    // 'Users.Social.login' => true,
+    // 'OAuth.providers.facebook.options.clientId' => 'YOUR APP ID',
+    // 'OAuth.providers.facebook.options.clientSecret' => 'YOUR APP SECRET',
+    // 'OAuth.providers.twitter.options.clientId' => 'YOUR APP ID',
+    // 'OAuth.providers.twitter.options.clientSecret' => 'YOUR APP SECRET',
+    //  'Users.Social.login' => true,
+];
