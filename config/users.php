98ccdfdc982c36429218b7651cb8dbbd3d120543
Diego Diaz <diego.leopoldo.diaz@gmail.com>
2022-04-06 23:01:54 -0400

    actions for users component

--- config/users.php
+++ config/users.php
@@ -1,6 +1,9 @@
 <?php
 return [
     'Users.Registration.active' => true,
+    'Users.Email.validate' => true,
+    'Auth.Identifiers.Password.fields.username' => 'email',
+    'Auth.Authenticators.Form.fields.username' => 'email',
     // 'Users.Social.login' => true,
     // 'OAuth.providers.facebook.options.clientId' => 'YOUR APP ID',
     // 'OAuth.providers.facebook.options.clientSecret' => 'YOUR APP SECRET',
